52ee27472f9505f058417cff05d9782241204b27
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-04 11:16:23 +0300

    profile model created

--- gram/models.py
+++ gram/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class
+class Profile(models.Model):
+    name = models.CharField(max_length=25)
+    username = models.CharField(max_length=25,unique=True)
+    bio = models.CharField(max_length=100)
+    profilepic = models.ImageField(upload_to='articles/',blank=True)
+    email = models.EmailField()
+    contact = models.CharField(max_length=15,blank=True)
