a8599789b25b0bd2612c5f9a5487d313f62e716d
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-04 11:44:10 +0300

    models functions added

--- gram/models.py
+++ gram/models.py
@@ -9,6 +9,30 @@ class Profile(models.Model):
     email = models.EmailField()
     contact = models.CharField(max_length=15,blank=True)
 
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ['username']
+
+    def save_profile(self):
+        self.save()
+
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=100)
+    commentator = models.ForeignKey(Profile)
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+
+
+
 
 class Image(models.Model):
     image = models.CharField(max_length=40)
@@ -16,9 +40,24 @@ class Image(models.Model):
     path = models.ImageField(upload_to='picture/', default=True)
     poster = models.ForeignKey(Profile)
     likes = models.IntegerField()
+    imagecomments = models.ForeignKey(Comment)
+    def save_image(self):
+        self.save()
 
+    def delete_image(self):
+        self.delete()
 
-class Comment(models.Model):
-    comment = models.CharField(max_length=100)
-    imagekey = models.ForeignKey(Image)
-    commentator = models.ForeignKey(Profile)
+    @classmethod
+    def get_image(cls,id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def getImages(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def image_comments(cls,id):
+        comments = cls.objects.filter(imagecomments__comment_id=id)
+        posters = cls.objects.filter(imagecomments__commnetator__id=id)
