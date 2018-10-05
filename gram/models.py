41f382b58da037fd467e37bf7d68bcbd53be93fd
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-05 17:54:06 +0300

    new profile template

--- gram/models.py
+++ gram/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from annoying.fields import AutoOneToOneField
 
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=25)
     username = models.CharField(max_length=25,unique=True)
-    account = models.ForeignKey(User,on_delete=models.CASCADE)
+    relate = models.ManyToManyField('self', symmetrical=False, through='Relationship')
     bio = models.CharField(max_length=100)
     profilepic = models.ImageField(upload_to='articles/',blank=True)
     email = models.EmailField()
     contact = models.CharField(max_length=15,blank=True)
-    following
+
 
     def __str__(self):
         return self.username
@@ -31,6 +32,9 @@ class Profile(models.Model):
         image = cls.objects.get(id=id)
         return image
 
+class Relationship(models.Model):
+    follow = models.ForeignKey(Profile, related_name="follow")
+    follower = models.ForeignKey(Profile, related_name="follower")
 
 
 
@@ -52,7 +56,7 @@ class Image(models.Model):
     image = models.CharField(max_length=40)
     caption = models.CharField(max_length=100)
     path = models.ImageField(upload_to='picture/', default=True)
-    poster = models.ForeignKey(Profile)
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.IntegerField()
     imagecomments = models.ForeignKey(Comment)
 
