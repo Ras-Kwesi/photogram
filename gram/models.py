7172ec59473bab872f4b516305bff96ddf8efece
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-05 11:52:14 +0300

    login and registartion features added

--- gram/models.py
+++ gram/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=25)
     username = models.CharField(max_length=25,unique=True)
+
     bio = models.CharField(max_length=100)
     profilepic = models.ImageField(upload_to='articles/',blank=True)
     email = models.EmailField()
